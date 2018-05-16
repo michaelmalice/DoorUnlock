d510e7962f86cee7c9490cb0276d6efbee371fd5
Michael Larsen <michael.larsen91@gmail.com>
2018-05-15 17:48:18 -0700

    Added comments to Requestor

--- Requestor.py
+++ Requestor.py
@@ -1,13 +1,17 @@
 import requests
 import threading
 
+# The Requestor class inherits from the Thread class so it can be run without holding the main thread
+# The Requestor class sends a web request to the Raspberry Pi telling it to run the servo and open the door
 class Requestor(threading.Thread):
 
+    # Read the URL of the server running on the Pi from a text file
     def __init__(self):
         threading.Thread.__init__(self)
         with open(r'/home/tme/unlockDoor.txt') as file:
             self.address = file.readline().rstrip('\n')
 
+    # The run function is called when the thread is started, it sends a request to the Pi to open the door
     def run (self):
         try:
             requests.get(self.address)
